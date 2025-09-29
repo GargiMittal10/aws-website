b1779068e4d8992b1c3536a793656cd830488fd3
GargiMittal10 <gargi.mittal.btech2022@sitpune.edu.in>
2025-09-29 20:09:01 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_PeEkpLgYi', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '60eueltq40732dpfq20d7hnab8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_l7VFoUhTe', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '76hoc95lq01soo8bs7d92uteou', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
